111273e3742946eeb5a650e11f46a0fd69349882
p56f <pawelcc@o2.pl>
2018-08-28 22:28:27 +0200

    Fix deployemnt configuration

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/worldclocks'));
+app.use(express.static(__dirname + '/dist/WorldClocks'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/worldclocks/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/WorldClocks/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
