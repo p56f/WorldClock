c0d5d97cfdfe0572f05541aeeae17d223c2e6c84
p56f <pawelcc@o2.pl>
2018-08-28 21:32:30 +0200

    Add simple text clock to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  private _currentTime: string;
+
+  get currentTime() {
+    return this._currentTime;
+  }
+
+  ngOnInit(): void {
+    setInterval(() => {this._currentTime = new Date().toLocaleTimeString()}, 1000);
+  }
 }
